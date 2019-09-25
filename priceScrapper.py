14d07b670119d91aabbaa252006be04bd1d4d338
Hendry <ruckshan@Ruckshans-MacBook-Pro.local>
2019-09-25 08:43:43 -0400

    more features with scrape added time py file

--- priceScrapper.py
+++ priceScrapper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import yagmail
 
-yag = yagmail.SMTP('hendryratnam07@gmail.com',oauth2_file='~/oauth2_creds.json')
+#yag = yagmail.SMTP('hendryratnam07@gmail.com',oauth2_file='~/oauth2_creds.json')
 url = 'https://www.newegg.com/p/pl?d=gpu'
 
 r = requests.get(url).text
@@ -19,7 +19,7 @@ for item in items:
     neweggItemNames.append(str(item.find('img')['title']))
 
 for item in itemAction:
-    neweggItemPrices.append(str(item.ul.find('li','price-current').strong))
+    neweggItemPrices.append(item.ul.find('li','price-current').strong.text.strip())
 
 wow = ""
 
@@ -28,8 +28,10 @@ contents = '''
 
 '''
 #yag.send(to='hendryratnam@gmail.com', contents=neweggItemNames.pop())
-while len(neweggItemNames) is not 0:
+while len(neweggItemNames) and len(neweggItemPrices) is not 0:
     wow = neweggItemNames.pop()
+    print(wow)
+    wow = neweggItemPrices.pop()
     print(wow)
     print('')
 
